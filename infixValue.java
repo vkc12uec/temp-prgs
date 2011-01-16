6d653c5dda590d1eedc429c091ce6f3a59d961d9
vivek chaurasiya <vivekchaurasiya@gmail.com>
2011-01-15 23:39:50 -0500

    adding new prgs

---
+++ infixValue.java
@@ -0,0 +1,62 @@
+import java.util.*;
+
+/* program to print infix to postfix
+ * TODO:	infix can contain ( or )
+ * 	evaluate an infix expression using 2 stacks
+ */
+public class infixValue {
+
+    public static void populatemap ( HashMap h) {
+        h.put('/', 2);
+        h.put('*', 2);
+        h.put('+', 1);
+        h.put('-', 1);
+        h.put('(', 0);
+        h.put(')', 0);
+    }
+
+    public static void main(String[] args) {
+        //System.out.println("Hello, World");
+        String s = "3+5*6-7*8+5";         // find its value
+        Stack soperand = new Stack ();
+        Stack soperator = new Stack ();
+        String postfix="";
+        HashMap precedence = new HashMap();
+        populatemap(precedence);
+
+        //System.out.println( precedence.get('/'));
+        for (int i=0;i<s.length();i++) {
+                Character c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                postfix+=(c);
+            }
+            else {  // its an operator
+                if (soperator.isEmpty())
+                    soperator.push (c);
+
+		else if ((Integer)precedence.get(soperator.peek()) < (Integer)precedence.get(c))
+                    soperator.push (c);
+
+                else {
+			int topofstack = (Integer)precedence.get(soperator.peek());
+			int pointer = (Integer)precedence.get(c);
+			System.out.println (" topofstck = "+topofstack + " pointer = " + pointer);
+			while ( (Integer)precedence.get(soperator.peek()) > (Integer)precedence.get(c)) {
+				postfix+=(soperator.pop());
+				if (soperator.isEmpty())
+					break;
+			}
+			soperator.push(c);
+                }
+            }
+		System.out.println ("i = "+ i + " char = "+c + " postfix = "+ postfix);
+        }		// for()
+
+	while (!soperator.isEmpty())
+		postfix+=(soperator.pop());
+
+	System.out.println (postfix);
+    }
+}
+

b871d12bf0a347086d2ba239517827d7c4a22b8b
vivek chaurasiya <vivekchaurasiya@gmail.com>
2011-01-16 13:22:49 -0500

    part 3 .... with some error in soperand Stack .... redo it

--- infixValue.java
+++ infixValue.java
@@ -6,63 +6,107 @@ import java.util.*;
  */
 public class infixValue {
 
-    public static void populatemap ( HashMap h) {
-        h.put('/', 2);
-        h.put('*', 2);
-        h.put('+', 1);
-        h.put('-', 1);
-        h.put('(', 0);
-        h.put(')', 0);
-    }
+	public static int evaluate ( int right, int left, char c) {
+		int res=0;
+		switch (c) {
+			case '/':
+				res = right/left;
+				break;
+			case '*':
+				res = right*left;
+				break;
+			case '+':
+				res = right+left;
+				break;
+			case '-':
+				res = right-left;
+				break;
+			default:
+				System.out.println (" Wrong operators");
+		}
+	return res;
+	}
 
-    public static void main(String[] args) {
-        //System.out.println("Hello, World");
-        //String s = "3+5*6-7*8+5";         // find its value	part1
-        String s = "3+5*6-7*(8+5)";         // find its value	part2
-        Stack soperand = new Stack ();
-        Stack soperator = new Stack ();
-        String postfix="";
-        HashMap precedence = new HashMap();
-        populatemap(precedence);
+	public static void populatemap ( HashMap h) {
+		h.put('/', 2);
+		h.put('*', 2);
+		h.put('+', 1);
+		h.put('-', 1);
+		h.put('(', 0);
+		h.put(')', 0);
+	}
 
-        //System.out.println( precedence.get('/'));
-        for (int i=0;i<s.length();i++) {
-                Character c = s.charAt(i);
+	public static void main(String[] args) {
+		//System.out.println("Hello, World");
+		//String s = "3+5*6-7*8+5";         // find its value	part1
+		String s = "3+5*6-7*(8+5)";         // find its value	part2
+		Stack soperand = new Stack ();
+		Stack soperator = new Stack ();
+		String postfix="";
+		int base=10;
+		HashMap precedence = new HashMap();
+		populatemap(precedence);
 
-            if (Character.isDigit(c)) {
-                postfix+=(c);
-            }
-            else {  // its an operator
-                if (soperator.isEmpty() || c=='(')
-                    soperator.push (c);
+		//System.out.println( precedence.get('/'));
+		for (int i=0;i<s.length();i++) {
+			char c = s.charAt(i);
 
-		else if (c==')') {
-			while ((soperator.peek()) != (Character)'(')
-				postfix+=(soperator.pop());
-			soperator.pop();
+			if (Character.isDigit(c)) {
+				//soperand.push(c);
+				Character.digit(c,base);
+				postfix+=(c);
+			}
+			else {  // its an operator
+				if (soperator.isEmpty() || c=='(')
+					soperator.push (c);
+
+				else if (c==')') {
+					while ((soperator.peek()) != (Character)'(') {
+						char op = (Character)soperator.pop();
+						int right = (Integer)soperand.pop();	//Character.digit((Character)soperand.pop(), base);
+						int left = (Integer)soperand.pop();	//Character.digit((Character)soperand.pop(), base);
+						int eval = evaluate(right, left, op);
+						soperand.push (eval);
+					}
+					postfix+=(soperator.pop());
+					soperator.pop();	// pop the '('
+				}
+
+				else if ((Integer)precedence.get(soperator.peek()) < (Integer)precedence.get(c))
+					soperator.push (c);
+
+				else {
+					int topofstack = (Integer)precedence.get(soperator.peek());
+					int pointer = (Integer)precedence.get(c);
+					System.out.println (" topofstck = "+topofstack + " pointer = " + pointer);
+
+					while ( (Integer)precedence.get(soperator.peek()) > (Integer)precedence.get(c)) {
+						char op = (Character)soperator.pop();
+						int right = (Integer)soperand.pop();	//Character.digit((Character)soperand.pop(), base);
+						int left = (Integer)soperand.pop();	//Character.digit((Character)soperand.pop(), base);
+						int eval = evaluate(right, left, op);
+						soperand.push (eval);
+						postfix+=(op);
+						if (soperator.isEmpty())
+							break;
+					}
+					soperator.push(c);
+				}
+			}
+			System.out.println ("i = "+ i + " char = "+c + " postfix = "+ postfix);
+		}		// for()
+
+		while (!soperator.isEmpty()) {
+			char op = (Character)soperator.pop();
+			int right = (Integer)soperand.pop();	//Character.digit((Character)soperand.pop(), base);
+			int left = (Integer)soperand.pop();	//Character.digit((Character)soperand.pop(), base);
+			int eval = evaluate(right, left, op);
+			soperand.push (eval);
+			postfix+=(op);
 		}
 
-		else if ((Integer)precedence.get(soperator.peek()) < (Integer)precedence.get(c))
-                    soperator.push (c);
-
-                else {
-			int topofstack = (Integer)precedence.get(soperator.peek());
-			int pointer = (Integer)precedence.get(c);
-			System.out.println (" topofstck = "+topofstack + " pointer = " + pointer);
-			while ( (Integer)precedence.get(soperator.peek()) > (Integer)precedence.get(c)) {
-				postfix+=(soperator.pop());
-				if (soperator.isEmpty())
-					break;
-			}
-			soperator.push(c);
-                }
-            }
-		System.out.println ("i = "+ i + " char = "+c + " postfix = "+ postfix);
-        }		// for()
-
-	while (!soperator.isEmpty())
-		postfix+=(soperator.pop());
-
-	System.out.println (postfix);
-    }
+		System.out.println (postfix);
+		System.out.println ("final value = " + (soperand.pop()));
+	}
 }
+

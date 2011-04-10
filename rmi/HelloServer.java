5104c6276b04f4e842c6b1247ebbcbdd9cce14ad
vivekchaurasiya <vivekchaurasiya@gmail.com>
2011-04-10 15:17:59 -0400

    kth largest element in 2 sorted arrays http://www.careercup.com/question?id=8461565

---
+++ rmi/HelloServer.java
@@ -0,0 +1,18 @@
+import java.rmi.*;
+import java.rmi.server.*;
+
+public class HelloServer {
+  /**
+   * Server program for the "Hello, world!" example.
+   * @param argv The command line arguments which are ignored.
+   */
+  public static void main (String[] argv) {
+    try {
+      Naming.rebind ("Hello", new Hello ("Hello, world!"));
+      System.out.println ("Hello Server is ready.");
+    } catch (Exception e) {
+      System.out.println ("Hello Server failed: " + e);
+    }
+  }
+
+}

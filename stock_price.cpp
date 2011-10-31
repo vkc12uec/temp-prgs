4710846b6c62469fd24ee5317b82cb7f01bf6887
vivekchaurasiya <vivekchaurasiya@gmail.com>
2011-10-30 22:15:35 -0400

    some code

--- stock_price.cpp
+++ stock_price.cpp
@@ -9,3 +9,55 @@ stock price is 12 - 1 = 11 but max loss is 9 -4 = 5
 // algo: O(n2):
 //       from position i, considerig a[i] to be max, find that min on its right which will
 //       give you lowest min ....
+
+/*
+Given an Array A={-2,4,30,-50,90,-60,100,120}
+The array index represents time of day.
+Say 0-9 A.M, 1- 10 A.M....etc
+And value represents stock price at that time.
+
+Get the max profit. i.e in this input,
+best buying price=-60
+best selling price=120
+
+15 
+*/
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main () {
+  int a[] = {5,4,3,2,1};
+  //int a[] = {-2,4,30,-50,90,-60,100,120};
+  int len = sizeof (a)/sizeof(int);
+  #if 0
+  cout << "\n1 :"<< sizeof (a);   // 32
+  int *p = a;
+  cout << "\n2 :"<< sizeof (p);   // 8
+  p = &a[0];
+  cout << "\n3 :"<< sizeof (p);   // 8
+  #endif
+  int min, max, lastProfit = INT_MIN;
+  min = (a[0]>a[1]) ? a[1] : a[0];
+  max = (a[0]>a[1]) ? a[0] : a[1];
+
+  if (min == a[0])
+    lastProfit = max - min;
+
+  cout <<"\n min | max | lastp "<< min << " "<< max << " "<<lastProfit;
+
+  for (int i=2 ; i<len ; i++) {
+    if (min > a[i]) {
+      min = a[i];
+      cout << "\n min changed = "<<min;
+      }
+    else {
+      lastProfit = (lastProfit > (a[i] - min) ) ? lastProfit : (a[i] - min);
+      cout << "\n lastProfit changed = "<<lastProfit;
+    }
+  }
+
+  cout << "\nfinal profit = "<< lastProfit;
+
+  return 0;
+}

bf726b0ec1bfa2fa83a1c5468269e787e65ab7dd
vivekchaurasiya <vivekchaurasiya@gmail.com>
2012-12-17 18:42:55 -0500

    some more data and drchrono challenge ... interseting ..

---
+++ area_histo.cpp
@@ -0,0 +1,18 @@
+
+/*
+  max area under histogram
+
+*/
+
+#include "topcoder_header.h"
+
+int main () {
+  int h[] = {9, 5, 2 , 1, 3, 5, 4, 8, 2, 7};
+  int len = SizeOfArray(h);
+
+  stack <int> grad;
+
+  for (int i=0; i<len; i++)
+
+  return 0;
+}
